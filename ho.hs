53a75ee8b1d2a5b349baf4fdd0c80c213c7a674c
David Sanders <dsanders@fusionbox.com>
2012-11-14 20:26:02 -0700

    Add getPosition

--- ho.hs
+++ ho.hs
@@ -22,7 +22,7 @@ posToChar White = 'O'
 newtype Line = Line { positions :: [Position] }
 
 instance Show Line where
-    show (Line ps) = intersperse '-' $ map posToChar ps
+    show (Line ps) = intersperse ' ' $ map posToChar ps
 
 newtype Board = Board { lines :: [Line] }
 
@@ -38,12 +38,29 @@ clear = void $ rawSystem cmd []
     where cmd = if os `elem` ["win32", "mingw32"] then "cls" else "clear"
 
 -- | Replaces an item at the specified index in a list.
-replaceAt :: (Integral i) => i -> a -> [a] -> [a]
+replaceAt :: (Integral a) => a -> b -> [b] -> [b]
 replaceAt _ _ [] = []
 replaceAt i r a@(x:xs)
     | i < 0      = a
     | i == 0     = r:xs
     | otherwise  = x:replaceAt (i - 1) r xs
+
+-- | Safely gets the head of a list.
+safeHead :: [a] -> Maybe a
+safeHead xs = if not $ null xs
+              then Just $ head xs
+              else Nothing
+
+-- | Safely gets an item in a list by index.
+safeIndex :: (Integral b) => [a] -> b -> Maybe a
+safeIndex [] _ = Nothing
+safeIndex (x:xs) i
+    | i < 0     = Nothing
+    | i == 0    = Just x
+    | otherwise = safeIndex xs (i - 1)
+
+(!!!) :: (Integral b) => [a] -> b -> Maybe a
+(!!!) = safeIndex
 
 ------------------------------------------------------------------------------
 -- Functions
@@ -80,6 +97,14 @@ isInside = (&&) `on` coordInRange
 isHoshi :: Int -> Int -> Bool
 isHoshi x y = (x, y) `elem` [(p, q) | p <- [3, 9, 15], q <- [3, 9, 15]]
 
+getPosition :: Char -> Char -> Board -> Maybe Position
+getPosition x y (Board b) = do
+    iX <- coord x
+    iY <- coord y
+    line <- b !!! iY
+    position <- positions line !!! iX
+    Just position
+
 setPosition :: Char -> Char -> Position -> Board -> Maybe Board
 setPosition x y p (Board b) = do
     iX <- coord x
@@ -91,6 +116,8 @@ setPosition x y p (Board b) = do
         newLine     = Line $ replaceAt iX newPosition (positions yLine)
         newBoard    = Board $ replaceAt iY newLine b
     return newBoard
+
+--  moveAt x y p (Board b) =
 
 ------------------------------------------------------------------------------
 -- Main
