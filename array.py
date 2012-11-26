251f911bc3a80515c980fd2141e005bf67b22c61
David Sanders <dsanders@fusionbox.com>
2012-11-26 15:16:44 -0700

    Add array.py

---
+++ array.py
@@ -0,0 +1,54 @@
+from copy import copy
+
+
+class Array(object):
+    class ArrayError(Exception):
+        pass
+
+    def __init__(self, width, height, empty=None):
+        self._width = width
+        self._height = height
+        self._empty = empty
+
+        self._reset()
+
+    def _reset(self, value=None):
+        value = value or self._empty
+
+        self._array = [
+            [value for i in range(self._width)]
+            for j in range(self._height)
+        ]
+
+    @property
+    def _copy(self):
+        return [copy(row) for row in self._array]
+
+    def _check_index(self, x, y):
+        if (
+            x < 1 or
+            x > self._width or
+            y < 1 or
+            y > self._height
+        ):
+            raise self.ArrayError(
+                'Index ({x}, {y}) is not within array dimensions {w}x{h}'.format(
+                    x=x, y=y, w=self._width, h=self._height
+                )
+            )
+
+    def _zero_index(cls, x, y):
+        return x - 1, y - 1
+
+    def __getitem__(self, i):
+        self._check_index(*i)
+        x, y = self._zero_index(*i)
+        return self._array[y][x]
+
+    def __setitem__(self, i, value):
+        self._check_index(*i)
+        x, y = self._zero_index(*i)
+        self._array[y][x] = value
+
+    def __eq__(self, other):
+        return self._array == other._array
