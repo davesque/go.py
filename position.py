8066baca67f2e774bec24d4867cdd94c01576692
David Sanders <dsanders@fusionbox.com>
2012-11-25 20:34:48 -0700

    Refactor position

--- position.py
+++ position.py
@@ -2,7 +2,7 @@ from utils import bold
 
 
 class Position(object):
-    COLORS = {
+    TYPES = {
         'black': bold('@'),
         'white': bold('O'),
         'empty': '.',
@@ -11,19 +11,22 @@ class Position(object):
     class PositionError(Exception):
         pass
 
-    def __init__(self, color):
-        if color not in self.COLORS:
-            raise self.PositionError('Color must be one of the following: {0}'.format(self.COLORS.keys()))
-        self._color = color
+    def __init__(self, type):
+        if type not in self.TYPES:
+            raise self.PositionError('Type \'{0}\' must be one of the following: {1}'.format(
+                type,
+                self.TYPES.keys(),
+            ))
+        self._type = type
 
     def __eq__(self, other):
-        return self._color == other._color
+        return self._type == other._type
 
     def __hash__(self):
-        return hash(self._color)
+        return hash(self._type)
 
     def __str__(self):
-        return self.COLORS[self._color]
+        return self.TYPES[self._type]
 
     def __repr__(self):
-        return self._color.title()
+        return self._type.title()
